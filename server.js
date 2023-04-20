fb796cb3f1f083cce26cfc1637f4bcb186071221
Clay Loftus <clayton.m.loftus@gmail.com>
2023-04-20 16:15:51 -0600

    Add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Setup 
 import express from 'express';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT;
 
